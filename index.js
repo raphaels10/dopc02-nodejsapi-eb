445e72b8206345e0e90a1fd9c9b448f196fb43e3
Raphael Almeida <raphaels10@hotmail.com>
2025-12-29 00:16:48 -0300

    add liveness route

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) => {
-    const message = {message: "Express app is running in version 1!"}
+    const message = {message: "Express app is running in version 2!"}
+    return res.status(200).json(message)
+})
+
+app.get("/liveness", (req, res) => {
+    const message = {status: "OK"}
     return res.status(200).json(message)
 })
 
